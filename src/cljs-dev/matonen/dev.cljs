d9936b9e5f61d8ee56c840131aa29544beab9b33
jarppe <jarppe@metosin.fi>
2014-12-01 23:31:32 +0200

    poll every 500 ms

--- src/cljs-dev/matonen/dev.cljs
+++ src/cljs-dev/matonen/dev.cljs
@@ -2,4 +2,4 @@
   (:require [lively :as l]))
 
 (js/console.log "dev setup")
-(l/start "/matonen-dev.js" {:polling-rate 5000 :on-reload (fn [] (js/console.log "reloaded"))})
+(l/start "/matonen-dev.js" {:polling-rate 500 :on-reload (fn [] (js/console.log "reloaded"))})
